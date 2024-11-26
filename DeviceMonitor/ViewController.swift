199eca7773f719a70ccd96717b8fb448fb6b4994
jeonyul <jeonyul00@gmail.com>
2024-11-26 10:05:54 +0900

    feat: ui

--- DeviceMonitor/ViewController.swift
+++ DeviceMonitor/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    
+    
 }
 
+
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "디바이스 이름"
+            cell.detailTextLabel?.text = UIDevice.current.name
+        default:
+            break
+        }
+        return cell
+    }
+    
+}
